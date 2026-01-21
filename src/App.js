cba9833a61dbf6f6effad45c81df1d4be79669b6
Deepik Malviya <122190050+malviyadeepika18@users.noreply.github.com>
2026-01-21 19:58:05 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Section1 from './component/Section';
 import Menu from './component/menu';
 import './component/style.css';
 import './bootstrap.css';
-import { BrowserRouter as Router, Route,Routes} from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './component/about';
 import Contact from './component/contact';
 import Reservation from './component/reservation';
@@ -14,22 +14,22 @@ import Gallery from './component/gallery';
 
 function App() {
   return (
-  
-  <div className="App">
-  
-<MyHeader />
-<Router>
+
+    <div className="App">
+
+      <MyHeader />
+      <Router>
         <Routes>
-          <Route path ="/" element={<Section1/>}/> 
-          <Route path ="/menu" element={<Menu/>}/> 
-          <Route path ="/about" element={<About/>}/> 
-          <Route path ="/contact" element={<Contact/>}/> 
-          <Route path ="/reservation" element={<Reservation/>}/> 
-          <Route path ="/gallery" element={<Gallery/>}/> 
-          
+          <Route path="/" element={<Section1 />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/reservation" element={<Reservation />} />
+          <Route path="/gallery" element={<Gallery />} />
         </Routes>
       </Router>
-  </div>
+
+    </div>
 
 
   );
